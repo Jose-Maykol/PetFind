891369289c1430fdf4bb1989fd04878f80f20460
José Maykol <jpaniura@unsa.edu.pe>
2023-10-27 08:15:30 -0500

    build(app): config theme next ui

--- petfind/tailwind.config.js
+++ petfind/tailwind.config.js
@@ -1,11 +1,43 @@
 /** @type {import('tailwindcss').Config} */
+
+const { nextui } = require('@nextui-org/react')
+const colors = require('tailwindcss/colors')
+
 export default {
   content: [
     './index.html',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+    colors: {
+      ...colors
+    },
     extend: {}
   },
-  plugins: []
+  darkMode: 'class',
+  plugins: [
+    nextui({
+      themes: {
+        dark: {
+          colors: {
+            primary: {
+              DEFAULT: '#F87171',
+              foreground: '#000000'
+            },
+            focus: '#F87171'
+          }
+        },
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: '#F87171',
+              foreground: '#FFFFFF'
+            },
+            focus: '#F87171'
+          }
+        }
+      }
+    })
+  ]
 }
