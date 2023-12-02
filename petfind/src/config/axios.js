4cb74e9b9a7f09b23b22b916192695c344684412
José Maykol <jpaniura@unsa.edu.pe>
2023-12-01 23:26:35 -0500

    feat(app): add axios configuration

---
+++ petfind/src/config/axios.js
@@ -0,0 +1,38 @@
+import { camelizeKeys, decamelizeKeys } from 'humps'
+import axios from 'axios'
+import { API_URL } from './config'
+
+const api = axios.create({
+  baseURL: API_URL
+})
+
+api.interceptors.response.use((response) => {
+  if (
+    response.data &&
+    response.headers['content-type'] === 'application/json; charset=utf-8'
+  ) {
+    response.data = camelizeKeys(response.data)
+  }
+  return response
+})
+
+api.interceptors.request.use((config) => {
+  const newConfig = { ...config }
+  newConfig.url = `${API_URL}${config.url}`
+
+  if (newConfig.headers['Content-Type'] === 'multipart/form-data') {
+    return newConfig
+  }
+
+  if (config.params) {
+    newConfig.params = decamelizeKeys(config.params)
+  }
+
+  if (config.data) {
+    newConfig.data = decamelizeKeys(config.data)
+  }
+
+  return newConfig
+})
+
+export default api
