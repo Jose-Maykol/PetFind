a8a3e51a53b5a8f922a3476be4983c05f21f75a8
José Maykol <jpaniura@unsa.edu.pe>
2023-12-07 11:44:50 -0500

    feat(app): add missing fields in report form

--- petfind/src/pages/Home/components/ReportFilters.jsx
+++ petfind/src/pages/Home/components/ReportFilters.jsx
@@ -5,55 +5,55 @@ import SearchIcon from '../../../components/Icons/SearchIcon'
 export default function ReportFilters () {
   return (
     <div className='w-1/3'>
-          <div className='flex flex-row items-end gap-2 p-4'>
-            <Input
-              label='Buscar por nombre'
-              labelPlacement='outside'
-              isClearable
-              radius='sm'
-              placeholder='Buscar'
-            />
-            <Button isIconOnly color='primary' aria-label='search'>
-              <SearchIcon width={24} height={24} fill='fill-white' />
-            </Button>
-          </div>
-          <div className='mt-6 border border-neutral-200 rounded-md p-4'>
-            <div className='flex flex-row items-end gap-2'>
-              <Input
-                label='Fecha de desaparicion'
-                labelPlacement='outside'
-                isClearable
-                radius='sm'
-                placeholder='Desde - Hasta'
-              />
-              <Button isIconOnly color='primary' aria-label='search-date'>
-                <CalendarIcon width={24} height={24} fill='fill-white' />
-              </Button>
-            </div>
-            <div className='py-4'>
-              <CheckboxGroup
-                label='Tipo de mascota'
-                classNames={{
-                  label: 'text-small font-medium text-black'
-                }}
-              >
-                <Checkbox classNames={{ label: 'text-small' }} value='gato'>Gato</Checkbox>
-                <Checkbox classNames={{ label: 'text-small' }} value='perro'>Perro</Checkbox>
-                <Checkbox classNames={{ label: 'text-small' }} value='ave'>Ave</Checkbox>
-                <Checkbox classNames={{ label: 'text-small' }} value='reptil'>Reptil</Checkbox>
-                <Checkbox classNames={{ label: 'text-small' }} value='roedor'>Roedor</Checkbox>
-                <Checkbox classNames={{ label: 'text-small' }} value='otro'>Otro</Checkbox>
-              </CheckboxGroup>
-            </div>
-            <div>
-              <Button
-                color='primary'
-                className='w-full'
-              >
-                Filtrar
-              </Button>
-            </div>
-          </div>
+      <div className='flex flex-row items-end gap-2 p-4'>
+        <Input
+          label='Buscar por nombre'
+          labelPlacement='outside'
+          isClearable
+          radius='sm'
+          placeholder='Buscar'
+        />
+        <Button isIconOnly color='primary' aria-label='search'>
+          <SearchIcon width={24} height={24} fill='fill-white' />
+        </Button>
+      </div>
+      <div className='mt-6 border border-neutral-200 rounded-md p-4'>
+        <div className='flex flex-row items-end gap-2'>
+          <Input
+            label='Fecha de desaparicion'
+            labelPlacement='outside'
+            isClearable
+            radius='sm'
+            placeholder='Desde - Hasta'
+          />
+          <Button isIconOnly color='primary' aria-label='search-date'>
+            <CalendarIcon width={24} height={24} fill='fill-white' />
+          </Button>
         </div>
+        <div className='py-4'>
+          <CheckboxGroup
+            label='Tipo de mascota'
+            classNames={{
+              label: 'text-small font-medium text-black'
+            }}
+          >
+            <Checkbox classNames={{ label: 'text-small' }} value='gato'>Gato</Checkbox>
+            <Checkbox classNames={{ label: 'text-small' }} value='perro'>Perro</Checkbox>
+            <Checkbox classNames={{ label: 'text-small' }} value='ave'>Ave</Checkbox>
+            <Checkbox classNames={{ label: 'text-small' }} value='reptil'>Reptil</Checkbox>
+            <Checkbox classNames={{ label: 'text-small' }} value='roedor'>Roedor</Checkbox>
+            <Checkbox classNames={{ label: 'text-small' }} value='otro'>Otro</Checkbox>
+          </CheckboxGroup>
+        </div>
+        <div>
+          <Button
+            color='primary'
+            className='w-full'
+          >
+            Filtrar
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
--- petfind/src/pages/ReportPage.jsx
+++ petfind/src/pages/ReportPage.jsx
@@ -1,35 +1,80 @@
-import { Input, Select } from '@nextui-org/react'
+import { Button, Input, Select, Textarea } from '@nextui-org/react'
+import CoinBagIcon from '../components/Icons/CoinBagIcon'
+import CalendarIcon from '../components/Icons/CalendarIcon'
 
 export default function ReportPage () {
   return (
     <div className='w-screen flex flex-col items-center justify-center pt-6'>
       <div className='w-[700px]'>
         <h2 className='font-bold text-lg'>Reportar mascota perdida</h2>
-        <div className='flex flex-col gap-4 py-4'>
-          <Input
-            type='text'
-            label='Nombre de la mascota'
-            labelPlacement='outside'
-            placeholder='Ingrese el nombre de la mascota'
-            classNames={{ label: 'text-neutral-400' }}
-            required
-          />
-          <Select
-            label='Tipo de mascota'
-            labelPlacement='outside'
-            placeholder='Seleccione el tipo de mascota'
-            classNames={{ label: 'text-neutral-400' }}
-            required
-          />
-          <Input
-            type='text'
-            label='Numero de contacto'
-            labelPlacement='outside'
-            placeholder='Ingrese el numero de contacto'
-            classNames={{ label: 'text-neutral-400' }}
-            required
-          />
-        </div>
+        <form action=''>
+          <div className='flex flex-col gap-4 py-4'>
+            <Input
+              type='text'
+              label='Nombre de la mascota'
+              labelPlacement='outside'
+              placeholder='Ingrese el nombre de la mascota'
+              classNames={{ label: 'text-neutral-400' }}
+              required
+            />
+            <Select
+              label='Tipo de mascota'
+              labelPlacement='outside'
+              placeholder='Seleccione el tipo de mascota'
+              classNames={{ label: 'text-neutral-400' }}
+              required
+            />
+            <Textarea
+              label='Descripcion'
+              labelPlacement='outside'
+              placeholder='Ingrese una descripcion de la mascota'
+              classNames={{ label: 'text-neutral-400' }}
+              required
+            />
+            <div className='flex flex-row gap-2 items-end'>
+              <Input
+                type='text'
+                label='Fecha de desaparicion'
+                labelPlacement='outside'
+                placeholder='Ingrese la fecha de desaparicion'
+                classNames={{ label: 'text-neutral-400' }}
+                required
+              />
+              <Button
+                color='primary'
+                isIconOnly
+                startContent={<CalendarIcon width={24} height={24} fill='fill-white' />}
+              />
+            </div>
+            <Input
+              type='text'
+              label='Numero de contacto'
+              labelPlacement='outside'
+              placeholder='Ingrese el numero de contacto'
+              classNames={{ label: 'text-neutral-400' }}
+              required
+            />
+            <div className='flex flex-row gap-2 items-end'>
+              <Input
+                type='text'
+                label='Recompensa'
+                labelPlacement='outside'
+                placeholder='Ingrese la recompensa'
+                classNames={{ label: 'text-neutral-400' }}
+                className='flex-1'
+                required
+              />
+              <Button
+                color='primary'
+                isIconOnly
+                startContent={<CoinBagIcon width={24} height={24} fill='fill-white' />}
+              />
+            </div>
+          </div>
+          <Button color='primary' className='w-full'>
+            Reportar
+          </Button>
+        </form>
       </div>
     </div>
   )
