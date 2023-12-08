6fe9d0e51a57cbc05faf90b9d7c02bd8ebc5ff3f
José Maykol <jpaniura@unsa.edu.pe>
2023-12-08 18:48:25 -0500

    feat(app): get cookies

--- petfind/src/components/NavBar.jsx
+++ petfind/src/components/NavBar.jsx
@@ -1,12 +1,36 @@
-import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react'
+import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem, User } from '@nextui-org/react'
 import GoogleIcon from './Icons/GoogleIcon'
 import { Link, Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Cookies from 'js-cookie'
+import { useQuery } from 'react-query'
+import UserService from '../services/UserService'
+import useAuthStore from '../store/useAuthStore'
 
 export default function NavBar () {
+  const [isLogged, setIsLogged] = useState(false)
+  const { setTokens } = useAuthStore()
+
   const handleLoginWithGoogle = () => {
     window.location.href = 'http://localhost:8000/auth/google'
     console.log('Login with Google')
   }
+
+  useEffect(() => {
+    const accessToken = Cookies.get('accessToken')
+    const jwtToken = Cookies.get('jwtToken')
+    if (accessToken && jwtToken) {
+      setIsLogged(true)
+      setTokens({ accessToken, jwtToken })
+    }
+  }, [])
+
+  const userQuery = useQuery('user', UserService.getInfoUser, {
+    enabled: isLogged,
+    retry: 2
+  })
+
+  const userInfo = userQuery.data?.data.userInfo
 
   return (
     <>
@@ -28,13 +52,23 @@ export default function NavBar () {
         </NavbarContent>
         <NavbarContent justify='end'>
           <NavbarItem>
-            <Button
-              startContent={<GoogleIcon width={24} height={24} />}
-              variant='bordered'
-              onClick={handleLoginWithGoogle}
-            >
-              <p className='font-semibold'>Iniciar sesión con Google</p>
-            </Button>
+            {(isLogged)
+              ? (
+                <User
+                  name={userInfo?.name}
+                  description={userInfo?.email}
+                  avatarProps={{ src: userInfo?.profilePicture }}
+                />
+                )
+              : (
+                <Button
+                  startContent={<GoogleIcon width={24} height={24} />}
+                  variant='bordered'
+                  onClick={handleLoginWithGoogle}
+                >
+                  <p className='font-semibold'>Iniciar sesión con Google</p>
+                </Button>
+                )}
           </NavbarItem>
         </NavbarContent>
       </Navbar>
