505bbcdb515bbbd6136eaea299f8a1239ebf79f7
José Maykol <jpaniura@unsa.edu.pe>
2023-12-11 11:39:41 -0500

    feat(app): include marker position and handle map click event

--- petfind/src/pages/ReportPage/components/ReportMap.jsx
+++ petfind/src/pages/ReportPage/components/ReportMap.jsx
@@ -1,5 +1,6 @@
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api'
 import { GOOGLE_MAPS_API_KEY } from '../../../config/config'
+import { useState } from 'react'
 
 export default function ReportMap () {
   const defaultLocation = {
@@ -7,15 +8,29 @@ export default function ReportMap () {
     lng: -71.5367660522461
   }
 
+  const [markerPosition, setMarkerPosition] = useState(defaultLocation)
+
+  const handleMapClick = (event) => {
+    console.log(event)
+    const lat = event.latLng.lat()
+    const lng = event.latLng.lng()
+    setMarkerPosition({ lat, lng })
+  }
+
   return (
-    <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY}>
-      <GoogleMap
-        mapContainerStyle={{ width: '100%', height: '400px' }}
-        zoom={15}
-        center={defaultLocation}
-      >
-        <Marker position={defaultLocation} />
-      </GoogleMap>
-    </LoadScript>
+    <div className='w-full h-[400px]'>
+      <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY}>
+        <GoogleMap
+          onClick={(event) => handleMapClick(event)}
+          mapContainerStyle={{ width: '100%', height: '400px' }}
+          zoom={15}
+          center={defaultLocation}
+        >
+          {markerPosition && <MarkerF position={{ lat: markerPosition.lat, lng: markerPosition.lng }} />}
+        </GoogleMap>
+      </LoadScript>
+      <input type='hidden' name='lat' value={markerPosition.lat} />
+      <input type='hidden' name='lng' value={markerPosition.lng} />
+    </div>
   )
 }
