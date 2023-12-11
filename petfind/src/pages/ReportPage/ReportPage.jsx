f675e841a10f8da319b77f9aad867396fe0cbe76
José Maykol <jpaniura@unsa.edu.pe>
2023-12-10 23:12:27 -0500

    feat(app): add upload pet photo

--- petfind/src/pages/ReportPage/ReportPage.jsx
+++ petfind/src/pages/ReportPage/ReportPage.jsx
@@ -6,6 +6,7 @@ import Calendar from 'react-calendar'
 import './ReportPage.css'
 import PetTypesService from '../../services/PetTypesService'
 import { useQuery } from 'react-query'
+import UploadPetPhoto from './components/UploadPetPhoto'
 
 export default function ReportPage () {
   const [isOpenCalendar, setIsOpenCalendar] = useState(false)
@@ -41,49 +42,54 @@ export default function ReportPage () {
         <h2 className='font-bold text-lg'>Reportar mascota perdida</h2>
         <form onSubmit={handleSubmit}>
           <div className='flex flex-col gap-4 py-4'>
-            <Input
-              type='text'
-              name='name'
-              label='Nombre de la mascota'
-              labelPlacement='outside'
-              placeholder='Ingrese el nombre de la mascota'
-              classNames={{ label: 'text-neutral-400' }}
-              required
-            />
-            <Select
-              label='Tipo de mascota'
-              name='petTypeId'
-              labelPlacement='outside'
-              placeholder='Seleccione el tipo de mascota'
-              classNames={{ label: 'text-neutral-400', value: 'capitalize' }}
-              required
-            >
-              {petTypes?.map((petType) => (
-                <SelectItem key={petType.id} value={petType.id} className='capitalize'>
-                  {petType.tag}
-                </SelectItem>
-              ))}
-            </Select>
-            <div className='flex flex-row gap-2 items-end'>
-              <Input
-                type='number'
-                name='age_years'
-                label='Edad de la mascota'
-                labelPlacement='outside'
-                placeholder='Años'
-                classNames={{ label: 'text-neutral-400' }}
-                required
-              />
-              <Input
-                type='number'
-                name='age_months'
-              // label='Nombre de la mascota'
-                labelPlacement='outside'
-                placeholder='Meses'
-                classNames={{ label: 'text-neutral-400' }}
-                required
-              />
+            <div className='flex flex-row gap-6'>
+              <UploadPetPhoto />
+              <div className='flex flex-col gap-4 py-4'>
+                <Input
+                  type='text'
+                  name='name'
+                  label='Nombre de la mascota'
+                  labelPlacement='outside'
+                  placeholder='Ingrese el nombre de la mascota'
+                  classNames={{ label: 'text-neutral-400' }}
+                  required
+                />
+                <Select
+                  label='Tipo de mascota'
+                  name='petTypeId'
+                  labelPlacement='outside'
+                  placeholder='Seleccione el tipo de mascota'
+                  classNames={{ label: 'text-neutral-400', value: 'capitalize' }}
+                  required
+                >
+                  {petTypes?.map((petType) => (
+                    <SelectItem key={petType.id} value={petType.id} className='capitalize'>
+                      {petType.tag}
+                    </SelectItem>
+                  ))}
+                </Select>
+                <div className='flex flex-row gap-2 items-end'>
+                  <Input
+                    type='number'
+                    name='age_years'
+                    label='Edad de la mascota'
+                    labelPlacement='outside'
+                    placeholder='Años'
+                    classNames={{ label: 'text-neutral-400' }}
+                    required
+                  />
+                  <Input
+                    type='number'
+                    name='age_months'
+                    labelPlacement='outside'
+                    placeholder='Meses'
+                    classNames={{ label: 'text-neutral-400' }}
+                    required
+                  />
+                </div>
+              </div>
             </div>
+
             <Textarea
               label='Descripcion'
               name='description'
@@ -101,7 +107,7 @@ export default function ReportPage () {
                 value={date.toLocaleDateString()}
                 onValueChange={setDate.toString()}
                 placeholder='Ingrese la fecha de desaparicion'
-                classNames={{ label: 'text-neutral-400' }}
+                classNames={{ label: 'text-neutral-400', input: 'text-neutral-400' }}
                 required
                 disabled
               />
@@ -134,7 +140,7 @@ export default function ReportPage () {
             />
             <div className='flex flex-row gap-2 items-end'>
               <Input
-                type='text'
+                type='number'
                 name='reward'
                 label='Recompensa'
                 labelPlacement='outside'
---
+++ petfind/src/pages/ReportPage/components/UploadPetPhoto.jsx
@@ -0,0 +1,56 @@
+import { Button } from '@nextui-org/react'
+import { useState } from 'react'
+import CloseIcon from '../../../components/Icons/CloseIcon'
+import UploadIcon from '../../../components/Icons/UploadIcon'
+
+export default function UploadPetPhoto () {
+  const [petPhoto, setPetPhoto] = useState(null)
+
+  const handlePhotoChange = (e) => {
+    const file = e.target.files[0]
+    if (file) {
+      const imageUrl = URL.createObjectURL(file)
+      setPetPhoto(imageUrl)
+    }
+  }
+
+  const handleResetPhoto = () => {
+    setPetPhoto(null)
+    const fileInput = document.getElementById('dropzone-file')
+    if (fileInput) {
+      fileInput.value = null
+    }
+  }
+
+  return (
+    <div className='flex items-center justify-center w-[300px]'>
+
+      <div className={`relative items-center justify-center w-[300px] ${petPhoto ? '' : 'hidden'}`}>
+        <img src={petPhoto} alt='pet' className='object-cover w-full h-64 rounded-lg' />
+        <Button
+          isIconOnly
+          className='absolute bottom-[10px] right-[10px] w-10 h-10 rounded-full bg-red-500'
+          onClick={handleResetPhoto}
+        >
+          <CloseIcon width={20} height={20} fill='fill-white' />
+        </Button>
+      </div>
+      <label htmlFor='dropzone-file' className={`flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 ${petPhoto ? 'hidden' : ''}`}>
+        <div className='flex flex-col items-center justify-center pt-5 pb-6'>
+          <UploadIcon />
+          <p className='mb-2 text-sm text-neutral-500'><span className='font-semibold'> Arrastra y suelta </span> la foto de tu mascota</p>
+          <p className='text-xs text-neutral-500'>SVG, PNG, JPG (MAX. 800x400px)</p>
+        </div>
+        <input
+          id='dropzone-file'
+          name='photo'
+          type='file'
+          className='hidden'
+          accept='image/*'
+          onChange={handlePhotoChange}
+        />
+      </label>
+
+    </div>
+  )
+}
