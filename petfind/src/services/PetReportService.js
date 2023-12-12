ed696dd04ecc8ee40a6d503f5b21d01cb0908d98
José Maykol <jpaniura@unsa.edu.pe>
2023-12-11 23:48:54 -0500

    feat(app): add create pet report to service

--- petfind/src/services/PetReportService.js
+++ petfind/src/services/PetReportService.js
@@ -1,9 +1,26 @@
+import Cookies from 'js-cookie'
 import api from '../config/axios'
 
 class PetReportService {
   async getPetReports (params) {
     try {
       const response = await api.get('/pet-reports', { params })
+      return response.data
+    } catch (error) {
+      return error.response.data
+    }
+  }
+
+  async createPetReport (data) {
+    try {
+      const jwtToken = Cookies.get('jwtToken')
+      const response = await api.post('/pet-reports/own', data, {
+        headers: {
+          ...api.defaults.headers,
+          Authorization: `Bearer ${jwtToken}`,
+          'Content-Type': 'multipart/form-data'
+        }
+      })
       return response.data
     } catch (error) {
       return error.response.data
