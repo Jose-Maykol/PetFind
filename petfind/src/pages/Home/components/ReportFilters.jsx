45b5a0734a01ef6b5b36e1e06bb38846d8da531c
José Maykol <jpaniura@unsa.edu.pe>
2023-12-17 17:44:42 -0500

    feat(app): add calendar functionality to report filters

--- petfind/src/pages/Home/components/ReportFilters.jsx
+++ petfind/src/pages/Home/components/ReportFilters.jsx
@@ -1,16 +1,26 @@
-import { Button, Checkbox, CheckboxGroup, Input } from '@nextui-org/react'
+import { Button, Checkbox, CheckboxGroup, Input, Popover, PopoverContent, PopoverTrigger } from '@nextui-org/react'
 import CalendarIcon from '../../../components/Icons/CalendarIcon'
 import SearchIcon from '../../../components/Icons/SearchIcon'
 import { useQuery } from 'react-query'
 import PetTypesService from '../../../services/PetTypesService'
+import Calendar from 'react-calendar'
+import { useState } from 'react'
 
 export default function ReportFilters () {
+  const [isOpenCalendar, setIsOpenCalendar] = useState(false)
+  // const [dates, setDates] = useState([new Date(Date.now() - 2 * 24 * 60 * 60 * 1000), new Date()])
+  const [dates, setDates] = useState([null, null]) // [startDate, endDate
+
   const petTypesQuery = useQuery('petTypes', PetTypesService.getAll, {
     retry: 2,
     staleTime: Infinity
   })
 
   const petTypes = petTypesQuery.data?.data.petTypes
+
+  const onChangeDate = (dates) => {
+    setDates(dates)
+  }
 
   return (
     <div className='w-full min-w-[250px] lg:w-1/3'>
@@ -30,14 +40,32 @@ export default function ReportFilters () {
         <div className='flex flex-row items-end gap-2'>
           <Input
             label='Fecha de desaparicion'
+            type='text'
+            disabled
             labelPlacement='outside'
+            value={dates[0] ? `${dates[0].toLocaleDateString()} - ${dates[1].toLocaleDateString()}` : ''}
+            onValueChange={(value) => setDates(value)}
             isClearable
             radius='sm'
             placeholder='Desde - Hasta'
+            classNames={{ label: 'text-neutral-400', input: 'text-neutral-400' }}
           />
-          <Button isIconOnly color='primary' aria-label='search-date'>
-            <CalendarIcon width={24} height={24} fill='fill-white' />
-          </Button>
+          <Popover placement='right' isOpen={isOpenCalendar} onOpenChange={(open) => setIsOpenCalendar(open)}>
+            <PopoverTrigger>
+              <Button isIconOnly color='primary' aria-label='search-date'>
+                <CalendarIcon width={24} height={24} fill='fill-white' />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className='px-2 pb-4'>
+              <Calendar
+                onChange={onChangeDate}
+                selectRange
+                value={dates}
+                prev2Label={null}
+                next2Label={null}
+              />
+            </PopoverContent>
+          </Popover>
         </div>
         <div className='py-4'>
           <CheckboxGroup
