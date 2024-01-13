0816ea652ed663d5612daebd667a7c1c5439deaf
José Maykol <jpaniura@unsa.edu.pe>
2024-01-13 02:29:24 -0500

    fix(app): heat map errors

--- petfind/src/pages/MyReports/components/ReportSightingMap.jsx
+++ petfind/src/pages/MyReports/components/ReportSightingMap.jsx
@@ -1,62 +1,35 @@
-import { useEffect, useState } from 'react'
-import PetReportService from '../../../services/PetReportService'
-import { useQuery } from 'react-query'
-import { GoogleMap, HeatmapLayer, HeatmapLayerF, MarkerF, useJsApiLoader } from '@react-google-maps/api'
+import { GoogleMap, HeatmapLayerF, MarkerF, useJsApiLoader } from '@react-google-maps/api'
 import { GOOGLE_MAPS_API_KEY } from '../../../config/config'
 import { PropTypes } from 'prop-types'
+import React, { useEffect } from 'react'
 
-export default function ReportSightingMap ({ idPetReport }) {
+export default function ReportSightingMap ({ coordinates }) {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: GOOGLE_MAPS_API_KEY,
     libraries: ['visualization']
   })
 
-  const [petReportSighting, setPetReportSighting] = useState(null)
-  const [coordinates, setCoordinates] = useState([])
-  const [markers, setMarkers] = useState([])
-  useQuery(['petReportSighting', idPetReport], () => {
-    if (idPetReport !== null) {
-      return PetReportService.getReportsSightings(idPetReport)
-    }
-    return null
-  }, {
-    retry: 2,
-    // staleTime: Infinity,
-    enabled: !!idPetReport,
-    onSuccess: (res) => {
-      setPetReportSighting(res.data)
-    }
-  })
+  const [heatMapData, setHeatMapData] = React.useState([])
 
-  useEffect(() => {
-    if (petReportSighting !== null) {
-      setCoordinates(
-        petReportSighting.map((report) => {
-          return new window.google.maps.LatLng(report.reportCoordinates.x, report.reportCoordinates.y)
-        })
-      )
-      setMarkers(
-        petReportSighting.map((report) => {
-          return {
-            position: new window.google.maps.LatLng(report.reportCoordinates.x, report.reportCoordinates.y),
-            title: report.userName
-          }
-        })
-      )
-    }
-  }, [petReportSighting])
+  console.log(coordinates)
 
   const defaultLocation = {
     lat: -16.3989200592041,
     lng: -71.5367660522461
   }
 
-  if (!petReportSighting) {
-    return null
-  }
+  useEffect(() => {
+    if (isLoaded && coordinates !== undefined && coordinates.length > 0) {
+      const heatMapData = coordinates.map((coordinate) => {
+        return new window.google.maps.LatLng(coordinate.lat, coordinate.lng)
+      })
+      setHeatMapData(heatMapData)
+      console.log(heatMapData)
+    }
+  }, [isLoaded, coordinates])
 
-  if (petReportSighting.length === 0) {
+  if (coordinates.length === 0) {
     return (
       <div className='w-full h-[400px] border-2 border-dashed rounded-md border-neutral-500 flex justify-center items-center'>
         <p>Sin avistamientos</p>
@@ -66,18 +39,17 @@ export default function ReportSightingMap ({ idPetReport }) {
 
   return (
     <div className='w-full h-[400px]'>
-      {isLoaded && petReportSighting &&
+      {isLoaded && heatMapData.length > 0 &&
         <GoogleMap
           mapContainerStyle={{ width: '100%', height: '400px' }}
           zoom={15}
           center={defaultLocation}
         >
-          <HeatmapLayerF data={coordinates} />
-          {markers.map((markers, index) => (
+          <HeatmapLayerF data={heatMapData} options={{ radius: 40 }} />
+          {coordinates.map((coordinate, index) => (
             <MarkerF
               key={index}
-              position={markers.position}
-              title={markers.title}
+              position={coordinate}
             />
           ))}
         </GoogleMap>}
