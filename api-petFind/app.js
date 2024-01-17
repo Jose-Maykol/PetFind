609b8a44356ec827091a975f933435e4b277649b
José Maykol <jpaniura@unsa.edu.pe>
2024-01-17 05:19:14 -0500

    refactor(app): update origin and callback URL

--- api-petFind/app.js
+++ api-petFind/app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8000
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   credentials: true
 }
 
--- api-petFind/config/passport.config.js
+++ api-petFind/config/passport.config.js
@@ -3,11 +3,12 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
+const URL_CALLBACK = process.env.URL_CALLBACK
 
 passport.use(new GoogleStrategy({
   clientID: CLIENT_ID,
   clientSecret: CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/google/callback',
+  callbackURL: URL_CALLBACK,
   scope: ['openid', 'email', 'profile'],
   accessType: 'offline'
 }, (accessToken, refreshToken, profile, done) => {
