a0d1c9f12a6bcba1d2c6e54e4d1941822c0456f3
José Maykol <jpaniura@unsa.edu.pe>
2024-01-17 06:20:23 -0500

    build(api): add SSL configuration to database connection

--- api-petFind/config/db.config.js
+++ api-petFind/config/db.config.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false },
   sslmode: 'require'
 })
 
