46f14ee56a41a99443bd0aed83d96a3f8a11149e
José Maykol <jpaniura@unsa.edu.pe>
2024-01-23 20:11:11 -0500

    fix(app): add loading spinner and display pet report details

--- petfind/src/pages/LostPetReport/LostPetReportPage.jsx
+++ petfind/src/pages/LostPetReport/LostPetReportPage.jsx
@@ -5,6 +5,7 @@ import formatDate from '../../utils/formatDate'
 import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api'
 import { GOOGLE_MAPS_API_KEY } from '../../config/config'
 import useAuthStore from '../../store/useAuthStore'
+import { Spinner } from '@nextui-org/react'
 
 export default function LostPetReportPage () {
   const { isLoged } = useAuthStore()
@@ -17,12 +18,12 @@ export default function LostPetReportPage () {
   const params = useParams()
   const { id } = params
 
-  const queryPetReport = useQuery(['petReportDetail', id], () => PetReportService.getPetReport(id), {
+  const { data, isLoading } = useQuery(['petReportDetail', id], () => PetReportService.getPetReport(id), {
     retry: 2,
     staleTime: Infinity
   })
 
-  const petReport = queryPetReport.data?.data.petReport
+  console.log(data)
 
   if (isLoged === false) {
     return (
@@ -30,13 +31,25 @@ export default function LostPetReportPage () {
     )
   }
 
+  if (isLoading) {
+    return (
+      <div className='w-screen max-w-full flex flex-col items-center justify-center py-6'>
+        <div className='w-[700px]'>
+          <div className='w-full h-[100px] flex flex-row flex-wrap items-center justify-center'>
+            <Spinner />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className='w-screen max-w-full flex flex-col items-center justify-center py-2'>
       <div className='max-w-[700px]'>
         <div className='flex flex-row justify-between pb-4'>
-          <h2 className='font-bold text-lg'>{petReport?.name}</h2>
+          <h2 className='font-bold text-lg'>{data.data.petReport.name}</h2>
           <Link
-            to={`/lost-pet-report/${petReport?.id}/new-sighting`}
+            to={`/lost-pet-report/${data.data.petReport.id}/new-sighting`}
             className='rounded-md px-4 py-2 font-bold text-white text-sm bg-[#F87272] hover:bg-[#F87272]'
           >
             Reportar avistamiento
@@ -46,49 +59,49 @@ export default function LostPetReportPage () {
           <div className='flex flex-col sm:flex-row gap-6 items-center'>
             <img
               className='w-[250px] h-[250px] object-cover rounded-md'
-              src={petReport?.photo}
-              alt={petReport?.name}
+              src={data.data.petReport.photo}
+              alt={data.data.petReport.name}
               width={250}
               height={250}
             />
             <div className='flex flex-col flex-1 gap-2 py-4'>
               <div>
                 <h3 className='font-semibold text-small'>Descripcion</h3>
-                <p className='flex flex-row rounded-lg bg-neutral-100 py-2 px-3 my-1'>{petReport?.description}</p>
+                <p className='flex flex-row rounded-lg bg-neutral-100 py-2 px-3 my-1'>{data.data.petReport.description}</p>
               </div>
               <div>
                 <h3 className='font-semibold text-small'>Tipo de mascota</h3>
-                <p className='rounded-lg bg-neutral-100 h-10 py-2 px-3 my-1'>{petReport?.petTypeId}</p>
+                <p className='capitalize rounded-lg bg-neutral-100 h-10 py-2 px-3 my-1'>{data.data.petReport.petTypeTag}</p>
               </div>
               <div className='flex flex-row gap-2 items-end justify-between'>
                 <div className='flex-1'>
                   <h3 className='font-semibold text-small'>Fecha de desaparicion</h3>
-                  <p className='rounded-lg bg-neutral-100 h-10 py-2 px-3 my-1'>{formatDate(petReport?.lossDate)}</p>
+                  <p className='rounded-lg bg-neutral-100 h-10 py-2 px-3 my-1'>{formatDate(data.data.petReport.lossDate)}</p>
                 </div>
                 <div>
                   <h3 className='font-semibold text-small'>Edad</h3>
-                  <p className='rounded-lg bg-neutral-100 h-10 py-2 px-3 my-1'>{`${petReport?.ageYears} años ${petReport?.ageMonths} meses`}</p>
+                  <p className='rounded-lg bg-neutral-100 h-10 py-2 px-3 my-1'>{`${data.data.petReport.ageYears} años ${data.data.petReport.ageMonths} meses`}</p>
                 </div>
               </div>
             </div>
           </div>
           <div>
             <h3 className='font-semibold text-small'>Telefono</h3>
-            <p className='flex flex-row rounded-lg bg-neutral-100 py-2 px-3 my-1'>{petReport?.phone}</p>
+            <p className='flex flex-row rounded-lg bg-neutral-100 py-2 px-3 my-1'>{data.data.petReport.phone}</p>
           </div>
           <div>
             <h3 className='font-semibold text-small'>Recompensa</h3>
-            <p className='flex flex-row rounded-lg bg-neutral-100 py-2 px-3 my-1'>{petReport?.reward}</p>
+            <p className='flex flex-row rounded-lg bg-neutral-100 py-2 px-3 my-1'>{data.data.petReport.reward}</p>
           </div>
           <div>
             <h3 className='font-semibold text-small'>Ubicacion</h3>
             {isLoaded &&
               <GoogleMap
                 mapContainerStyle={{ width: '100%', height: '400px' }}
-                center={{ lat: petReport?.coordinates.x, lng: petReport?.coordinates.y }}
+                center={{ lat: data.data.petReport.coordinates.x, lng: data.data.petReport.coordinates.y }}
                 zoom={15}
               >
-                <MarkerF position={{ lat: petReport?.coordinates.x, lng: petReport?.coordinates.y }} />
+                <MarkerF position={{ lat: data.data.petReport.coordinates.x, lng: data.data.petReport.coordinates.y }} />
               </GoogleMap>}
           </div>
         </div>
